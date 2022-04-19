1b4a2ad19dcc1493c7eddf48e8c342383ac307a4
Jackie Li <jackie.li@target-energysolutions.com>
2022-04-19 11:44:31 +0100

    fix warnings

--- Formula/rofi.rb
+++ Formula/rofi.rb
@@ -1,22 +1,22 @@
 class Rofi < Formula
-  desc "A window switcher, application launcher and dmenu replacement"
+  desc "Window switcher, application launcher and dmenu replacement"
   homepage "https://github.com/davatorium/rofi"
   url "https://github.com/davatorium/rofi/releases/download/1.7.3/rofi-1.7.3.tar.gz"
   sha256 "608458775d0a8699dc31027e786aa9f3a63658ebfb98c5c807620a5889687ecc"
   license "MIT"
   head "https://github.com/davatorium/rofi.git", branch: "next"
 
-  depends_on "cmake" => :build
   depends_on "bison" => :build
+  depends_on "cmake" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "flex"
   depends_on "cairo"
+  depends_on "flex"
   depends_on "gdk-pixbuf"
-  depends_on "pango"
   depends_on "glib"
-  depends_on "libxkbcommon"
   depends_on "libxcb"
+  depends_on "libxkbcommon"
+  depends_on "pango"
   depends_on "startup-notification"
   depends_on "xcb-util"
   depends_on "xcb-util-cursor"
