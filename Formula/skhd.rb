47c69ff328dbfb539d3ac2fdb9631777e5a909c0
Jackie Li <jackie.space@gmail.com>
2023-11-02 09:08:39 +0000

    new skhd

--- Formula/skhd.rb
+++ Formula/skhd.rb
@@ -1,8 +1,8 @@
 class Skhd < Formula
   desc "Simple hotkey-daemon for macOS."
   homepage "https://github.com/jackielii/skhd"
-  url "https://github.com/jackielii/skhd/archive/refs/tags/v0.3.9.zip"
-  sha256 "d31395c87b26c33a8eb9f086846b5c7d145ab01a2351ee3f752a1bae30759263"
+  url "https://github.com/jackielii/skhd/archive/refs/tags/v0.3.9a.zip"
+  sha256 "7890a2e30ae37bd8575a9c360f9ea150dca4e5dd842dd833866464dc12246998"
   head "https://github.com/jackielii/skhd.git"
 
   def install
